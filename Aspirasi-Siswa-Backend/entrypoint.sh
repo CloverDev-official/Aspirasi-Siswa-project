2d306b8a09246c98e7bbc1cbdd044fb0a8482788
Dhika-Kcaw <muhammad.dhika114@gmail.com>
2026-05-12 23:03:41 +0800

    Update agar entrypoint.sh dijalanin dan juga .env kebaca atau kesalin

--- Aspirasi-Siswa-Backend/entrypoint.sh
+++ Aspirasi-Siswa-Backend/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ ! -f /app/.env ]; then
+    echo ".env not found, copying from .env.example"
+    cp /app/.env.example /app/.env
+fi
+
 if [ ! -f /app/.initialized ]; then
     echo "First run setup..."
     ./migrate_app seed-admin
